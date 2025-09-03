99b00c9a1b8277df0524e411860af8167455af54
Pavlo Satyrenko <zevsp09@gmail.com>
2025-09-03 12:52:44 +0300

    refactor: streamline trimming of input fields in auth validators

--- src/middlewares/auth.validator.ts
+++ src/middlewares/auth.validator.ts
@@ -3,39 +3,39 @@ import { body, cookie } from "express-validator";
 export const authValidator = {
     signUpValidator: [
         body("email")
+            .trim()
             .notEmpty().withMessage("Email is required")
             .isEmail().withMessage("Please provide a valid email address")
-            .trim()
             .normalizeEmail(),
         body("name")
+            .trim()
             .notEmpty().withMessage("Name is required")
             .isString().withMessage("Name must be a string")
             .isLength({ min: 2, max: 100 }).withMessage("Name must be between 2 and 100 characters")
-            .trim()
             .escape(),
         body("surname")
+            .trim()
             .notEmpty().withMessage("Surname is required")
             .isString().withMessage("Surname must be a string")
             .isLength({ min: 2, max: 100 }).withMessage("Surname must be between 2 and 100 characters")
-            .trim()
             .escape(),
         body("password")
+            .trim()
             .notEmpty().withMessage("Password is required")
             .isString().withMessage("Password must be a string")
             .isLength({ min: 6, max: 100 }).withMessage("Password must be between 6 and 100 characters")
-            .trim()
     ],
     loginValidator: [
         body("email")
+            .trim()
             .notEmpty().withMessage("Email is required")
             .isEmail().withMessage("Please provide a valid email address")
-            .trim()
             .normalizeEmail(),
         body("password")
+            .trim()
             .notEmpty().withMessage("Password is required")
             .isString().withMessage("Password must be a string")
             .isLength({ min: 6, max: 100 }).withMessage("Password must be between 6 and 100 characters")
-            .trim()
     ],
     refreshTokenValidator: [
         cookie("__Secure-RefreshToken")
